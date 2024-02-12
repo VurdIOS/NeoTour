39d131f407206ddedd821211770ddffd4e03b658
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2024-02-12 19:31:10 +0300

    Initial Commit

---
+++ NeoTour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoTour
+//
+//  Created by Камаль Атавалиев on 12.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
