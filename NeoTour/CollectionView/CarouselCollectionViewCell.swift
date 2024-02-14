f54d1b6bc2b46beb1c16bed7ad92fd20a0d0b77b
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2024-02-14 21:53:04 +0300

    -Create mainView -Setup Navtitle for it -Create and setup UicollectionView for carousel with categories of tours in top of View

---
+++ NeoTour/CollectionView/CarouselCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CarouselCollectionViewCell.swift
+//  NeoTour
+//
+//  Created by Камаль Атавалиев on 14.02.2024.
+//
+
+import UIKit
+
+class CarouselCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ NeoTour/Resources/MockFiles.swift
@@ -0,0 +1,8 @@
+//
+//  MockFiles.swift
+//  NeoTour
+//
+//  Created by Камаль Атавалиев on 14.02.2024.
+//
+
+import Foundation
